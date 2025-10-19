edcbe1d5d2dd6ab3cf1a80ae908badd338396812
rafayet <78610949+rafayet-git@users.noreply.github.com>
2025-10-18 21:00:35 -0400

    Added maps libraries, updated env naming

--- web/src/lib/google.ts
+++ web/src/lib/google.ts
@@ -1,3 +1,3 @@
 import { Client } from "@googlemaps/google-maps-services-js";
 export const googleClient = new Client({});
-export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY!;
+export const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!;
