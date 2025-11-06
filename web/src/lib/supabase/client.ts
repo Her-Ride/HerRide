335adabb3117ca43f87ada93c34825ea75358906
rafayet <78610949+rafayet-git@users.noreply.github.com>
2025-11-06 01:05:15 -0500

    Add new user table creation route

--- web/src/lib/supabase/client.ts
+++ web/src/lib/supabase/client.ts
@@ -1,16 +1,14 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { auth } from "@clerk/nextjs/server";
+import { auth } from '@clerk/nextjs/server'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const createClient = () =>
-  createBrowserClient(
-    supabaseUrl!,
-    supabaseKey!,
+export function createServerSupabaseClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      accessToken: async () => {
-        return (await auth()).getToken({ template: "supabase" });
+      async accessToken() {
+        return (await auth()).getToken()
       },
-    }
-  );
+    },
+  )
+}
